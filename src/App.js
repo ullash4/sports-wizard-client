f854cce1b95fa64a5475208eadfdd25e7ce8465d
ullash4 <programmerullash@gmail.com>
2022-05-01 23:28:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Pages/Blog/Blog';
+import Home from './Components/Pages/Home/Home';
+import ManageInventory from './Components/Pages/ManageInventory/ManageInventory';
+import Footer from './Components/SharedPages/Footer/Footer';
+import Header from './Components/SharedPages/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/inventory' element={<ManageInventory></ManageInventory>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+
+const Home = () => {
+    return (
+        <div>
+            <Carousel></Carousel>
+            <h1>this is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
