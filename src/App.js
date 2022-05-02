f4f3aa3c8adaefb19bf4789050395e1f6182e169
ullash4 <programmerullash@gmail.com>
2022-05-03 03:53:32 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Pages/Blog/Blog';
+import Home from './Components/Pages/Home/Home';
+import Header from './Components/SharedPages/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is new app</h1>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+      </Routes>
     </div>
   );
 }
